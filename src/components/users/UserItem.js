58e9022fd444da16e0b66cb0461239ea9b5f7d9f
github username <github email address>
2020-04-05 05:05:42 -0400

    Start implementation of UserItem compoent

---
+++ src/components/users/UserItem.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class UserItem extends Component {
+  state = {
+    id: 1,
+    login: 'mojombo',
+    avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+    html_url: 'https://github.com/mojombo'
+  };
+
+  render() {
+    const { id, login, avatar_url, html_url } = this.state;
+    return (
+      <div className='card text-center'>
+        <img className='round-img' src={avatar_url} style={{ width: '60px' }} />
+        <h3>{login}</h3>
+        <div>
+          <a href={html_url} className='btn btn-dark btn-sm my-1'>
+            Github Profile
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default UserItem;
