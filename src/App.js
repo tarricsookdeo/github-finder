f973bf4a3f2bc500ee7cef6e84685718ffaed3e7
Tarric Sookdeo <tarricsookdeo@outlook.com>
2020-04-05 22:39:26 -0400

    Add Github api key

--- src/App.js
+++ src/App.js
@@ -7,12 +7,15 @@ import Users from './components/users/Users';
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
   };
 
   async componentDidMount() {
     this.setState({ loading: true });
-    const res = await axios.get('https://api.github.com/users');
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
+      client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
     this.setState({ users: res.data, loading: false });
   }
 
