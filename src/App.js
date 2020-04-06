bc8402e0966ca230296997f61720a1db9085bec0
Tarric Sookdeo <tarricsookdeo@outlook.com>
2020-04-05 22:11:07 -0400

    Make real request to Github API

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get('https://api.github.com/users');
+    this.setState({ users: res.data, loading: false });
+  }
+
   render() {
     return (
-      <div className='App'>
+      <div className="App">
         <Navbar />
-        <div className='container'>
-          <Users />
+        <div className="container">
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
