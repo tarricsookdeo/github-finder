0c98c2602a42dc62c102a0563ca0e2865bf2934e
github username <github email address>
2020-04-05 21:27:02 -0400

    Convert Navbar and UserItem to functional components

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,20 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class UserItem extends Component {
-  render() {
-    const { login, avatar_url, html_url } = this.props.user;
-    return (
-      <div className='card text-center'>
-        <img className='round-img' src={avatar_url} style={{ width: '60px' }} />
-        <h3>{login}</h3>
-        <div>
-          <a href={html_url} className='btn btn-dark btn-sm my-1'>
-            Github Profile
-          </a>
-        </div>
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+  return (
+    <div className='card text-center'>
+      <img
+        className='round-img'
+        alt='User Icon'
+        src={avatar_url}
+        style={{ width: '60px' }}
+      />
+      <h3>{login}</h3>
+      <div>
+        <a href={html_url} className='btn btn-dark btn-sm my-1'>
+          Github Profile
+        </a>
       </div>
-    );
-  }
+    </div>
+  );
+};
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired
 }
 
 export default UserItem;
