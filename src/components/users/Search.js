5b2829fa99ffe8e1d8213fae4c944e0da604ba76
Tarric Sookdeo <tarricsookdeo@outlook.com>
2020-04-06 19:45:55 -0400

    Create github context api state

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,39 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import PropTypes from 'prop-types';
+import GithubContext from '../../context/github/githubContext';
 
-const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
+const Search = ({ showClear, clearUsers, setAlert }) => {
+  const githubContext = useContext(GithubContext);
+
   const [text, setText] = useState('');
 
-  const onSubmit = e => {
+  const onSubmit = (e) => {
     e.preventDefault();
     if (text === '') {
       setAlert('Please enter something', 'light');
     } else {
-      searchUsers(text);
+      githubContext.searchUsers(text);
       setText('');
     }
   };
 
-  const onChange = e => setText(e.target.value);
+  const onChange = (e) => setText(e.target.value);
 
   return (
     <div>
-      <form className='form' onSubmit={onSubmit}>
+      <form className="form" onSubmit={onSubmit}>
         <input
-          type='text'
-          name='text'
-          placeholder='Search Users...'
+          type="text"
+          name="text"
+          placeholder="Search Users..."
           value={text}
           onChange={onChange}
         />
         <input
-          type='submit'
-          value='Search'
-          className='btn btn-dark btn-block'
+          type="submit"
+          value="Search"
+          className="btn btn-dark btn-block"
         />
       </form>
       {showClear && (
-        <button className='btn btn-light btn-block' onClick={clearUsers}>
+        <button className="btn btn-light btn-block" onClick={clearUsers}>
           Clear
         </button>
       )}
@@ -42,10 +45,9 @@ const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
 };
 
 Search.propTypes = {
-  searchUsers: PropTypes.func.isRequired,
   clearUsers: PropTypes.func.isRequired,
   showClear: PropTypes.bool.isRequired,
-  setAlert: PropTypes.func.isRequired
+  setAlert: PropTypes.func.isRequired,
 };
 
 export default Search;
