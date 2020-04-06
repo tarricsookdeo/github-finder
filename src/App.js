39e146e55c8e1757ceb52aac643e90797be9565f
Tarric Sookdeo <tarricsookdeo@outlook.com>
2020-04-05 22:53:50 -0400

    Add Search component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
 
 class App extends Component {
   state = {
@@ -24,6 +25,7 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <div className="container">
+          <Search />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
